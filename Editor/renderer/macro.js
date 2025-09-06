a0ac9a354b5598bb54aae108bcebc7aa83ab71a0
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-06 02:23:14 -0400

    Macro in progress

--- Editor/renderer/macro.js
+++ Editor/renderer/macro.js
@@ -9,13 +9,13 @@ const createMacroProcessor = (editor, stateIndicator) => {
     const getState = target => {
         return {
             length: target.textLength,
-            selection: [target.selectionStart, target.selectionEnd],
+            position: target.selectionStart,
         }
     }; //getState
     const getDelta = (older, newer) => {
         return { // delta
             length: newer.length - older.length,
-            selection: [newer.selection[0] - older.selection[0], newer.selection[1] - older.selection[1]],
+            position: newer.position - older.position,
         };
     } //getDelta
 
@@ -24,13 +24,11 @@ const createMacroProcessor = (editor, stateIndicator) => {
         const currentState = getState(event.target);
         const delta = getDelta(previousState, currentState);
         previousState = currentState;
-        let action;
-        let data = event.data;
+        let data = event.data ? event.data : "";
         if (!data && event.inputType == "insertLineBreak") //special case, ugly one
             data = "\n";
         macro.push({
                     data: data,
-                    action: action,
                     state: currentState,
                     delta: delta,
                 });
@@ -38,32 +36,16 @@ const createMacroProcessor = (editor, stateIndicator) => {
 
     const playMacro = () => {
         if (recordingMacro) return;
-        for (const element of macro) { // SA??? not fully implemented
+        for (const element of macro) {
             const dataLength = element.data ? element.data.length : 0;
             let currentState = getState(editor);
             const added = element.delta.length - dataLength;
-            if (added == 0) {
-                if (!element.data)
-                    element.data = "";
-                editor.setSelectionRange(
-                    currentState.selection[0] + element.delta.selection[0],
-                    currentState.selection[1] + element.delta.selection[1],);
+            const range = currentState.position + element.delta.position;
+            editor.setSelectionRange(range, range);
+            if (added == 0)
                 editor.setRangeText(element.data);
-            } else {
-                if (!element.data)
-                    element.data = "";
-                let from = currentState.selection[0];
-                let to = currentState.selection[0] + added;
-                if (to < from) {
-                    const tmp = to;
-                    to = from;
-                    from = tmp;
-                } //if
-                editor.setRangeText(element.data, from, to);
-                editor.setSelectionRange(
-                    currentState.selection[0] + element.delta.selection[0],
-                    currentState.selection[1] + element.delta.selection[1],);
-            } //if delete
+            else
+                editor.setRangeText(element.data, range, range  - element.delta.length);
             currentState = getState(editor);
         } //loop
     }; //playMacro
