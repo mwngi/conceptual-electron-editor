33582a5b65a8e65cec119582ddf949a17feeea6f
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-05 23:36:58 -0400

    Macro under development TODO: issues with deletes in the middle

--- Editor/renderer/macro.js
+++ Editor/renderer/macro.js
@@ -4,56 +4,67 @@ const createMacroProcessor = (editor, stateIndicator) => {
 
     let recordingMacro = false;
     let macro = [];
+    let previousState = null;
 
-    const getState = (target, newState) => {
-        if (newState)
-            return { // delta
-                length: newState.length - target.length,
-                selection: [ newState.selection[0] - target.selection[0], newState.selection[1] - target.selection[1] ],
-            };
-        else
-            return {
-                length: target.textLength,
-                selection: [ target.selectionStart, target.selectionEnd ],
-    }}; //getState
+    const getState = target => {
+        return {
+            length: target.textLength,
+            selection: [target.selectionStart, target.selectionEnd],
+        }
+    }; //getState
+    const getDelta = (older, newer) => {
+        return { // delta
+            length: newer.length - older.length,
+            selection: [newer.selection[0] - older.selection[0], newer.selection[1] - older.selection[1]],
+        };
+    } //getDelta
 
     editor.addEventListener(definitionSet.events.input, event => {
-        if (!recordingMacro) return;
+        if (!recordingMacro) return;        
+        const currentState = getState(event.target);
+        const delta = getDelta(previousState, currentState);
+        previousState = currentState;
         let action;
-        if (event.inputType.startsWith("insert")) //SA??? see https://w3c.github.io/input-events/#interface-InputEvent-Attributes
-            action = "+";
-        else if (event.inputType.startsWith("delete")) //SA???
-            action = "-";
-        else
-            action = "";
         let data = event.data;
         if (!data && event.inputType == "insertLineBreak") //special case, ugly one
             data = "\n";
         macro.push({
                     data: data,
                     action: action,
-                    state: getState(event.target),
+                    state: currentState,
+                    delta: delta,
                 });
-    }); //editor.onkeydown
+    }); //editor input
 
     const playMacro = () => {
         if (recordingMacro) return;
-        let initialState = getState(editor);
-        let delta = null;
-        for (const element of macro) { // SA??? not fully implemented            
-            if (!delta)
-                delta = getState(element.state, initialState);
-            if (element.action == "+") {
+        for (const element of macro) { // SA??? not fully implemented
+            const dataLength = element.data ? element.data.length : 0;
+            let currentState = getState(editor);
+            const added = element.delta.length - dataLength;
+            if (added == 0) {
                 if (!element.data)
-                    element.data = "?";
+                    element.data = "";
+                editor.setSelectionRange(
+                    currentState.selection[0] + element.delta.selection[0],
+                    currentState.selection[1] + element.delta.selection[1],);
                 editor.setRangeText(element.data);
-                if (element.data)
-                    editor.setSelectionRange(
-                        element.state.selection[0] + delta.selection[0] + element.data.length,
-                        element.state.selection[1] + delta.selection[1] + element.data.length,)
-            } else if (macro.action == "-") {
-
+            } else {
+                if (!element.data)
+                    element.data = "";
+                let from = currentState.selection[0];
+                let to = currentState.selection[0] + added;
+                if (to < from) {
+                    const tmp = to;
+                    to = from;
+                    from = tmp;
+                } //if
+                editor.setRangeText(element.data, from, to);
+                editor.setSelectionRange(
+                    currentState.selection[0] + element.delta.selection[0],
+                    currentState.selection[1] + element.delta.selection[1],);
             } //if delete
+            currentState = getState(editor);
         } //loop
     }; //playMacro
 
@@ -67,11 +78,14 @@ const createMacroProcessor = (editor, stateIndicator) => {
             : (macro.length > 0
                 ? definitionSet.status.macroAvailable
                 : null);
+        if (on)
+            previousState = getState(editor);
     } //setRecordingState
     const canRecord = () => !recordingMacro;
     const canStopRecording = () => recordingMacro;
     const canPlay = () => !recordingMacro && macro.length > 0;
 
+    // Hot keys Ctrl+Shift+R (record start/stop), Ctrl+Shift+P (play macro):
     window.addEventListener(definitionSet.events.keydown, event => {
         if (event.shiftKey && event.ctrlKey) {
             if (event.code == definitionSet.keys.KeyP && canPlay()) {
